b20c5f5697ab5c9b27b06087f968140bea43cdcb
paulfgleeson <53836498+paulfgleeson@users.noreply.github.com>
2019-08-18 14:00:29 +0100

    Add files via upload

---
+++ ScheduleR.R
@@ -0,0 +1,24 @@
+
+# load libraries needed to do the task
+library(taskscheduleR)
+
+# creating a variable to find the R script that we want to run
+# extdata is in the taskscheduleR package folder which is where I stored the script "Dublin_Bike_API_Call.R"
+myscript <- system.file("extdata", "Dublin_Bike_API_Call.R", package = "taskscheduleR")
+
+# creating a variable to define when to start our process
+runon <- format(Sys.time() + 62, "%H:%M")
+
+# creating the task
+taskscheduler_create(taskname = "dublinbikecall", rscript = myscript, 
+                     schedule = "HOURLY", starttime = runon)
+
+# creating a variable to see all tasks in the task manager
+# x <-taskscheduleR::taskscheduler_ls()
+
+# viewing the df containing all tasks
+# View(x)
+
+# command to delete the script you created, if necessary
+# taskscheduler_delete("dublinbikecall")
+
